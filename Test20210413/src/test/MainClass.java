8a9f3b8addffa070b9951ded6bcda140c98a9525
user <user@DESKTOP-8MI5VJJ>
2021-04-13 09:11:56 +0900

    Se 클래스 추가&Main 클래스 print 작성

--- Test20210413/src/test/MainClass.java
+++ Test20210413/src/test/MainClass.java
@@ -1,10 +1,11 @@
-package Test;
+package test;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello Git");
+		System.out.println("첫번째 클래스");
 	}
 
 }
